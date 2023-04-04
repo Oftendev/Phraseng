2d4af1ad87f1f9485879373f9a443d28be891e71
Oftendev <karim.m.2004@gmail.com>
2023-04-04 21:05:01 +0300

    Добавил JSDoc документацию

--- dictionary_scripts/phrase_generator1.js
+++ dictionary_scripts/phrase_generator1.js
@@ -1,12 +1,30 @@
+/**
+ * @file Генерирует предложения в Present Simple
+ * @author Oftendev
+ */
+/** Местоимения, после которых не ставится 'S' в глаголе
+ * @type {Array<string>}
+ */
 var pronouns_without_s = ["I", "We", "You", "They"];
+/** Местоимения, после которых ставится 'S' в глаголе
+ * @type {Array<string>}
+ */
 var pronouns_with_s = ["He", "She", "It"];
-var verbs = [["have", "has"], ["can", "can"], ["make", "makes"], ["go", "goes"], ["say", "says"]];
+/** 5 глаголов, каждый в форме без 'S' и с 'S'
+ * @type {string[][]}
+ */
+var verbs = [
+  ["have", "has"],
+  ["can", "can"],
+  ["make", "makes"],
+  ["go", "goes"],
+  ["say", "says"],
+];
 for (const i of verbs) {
-    for (const k of pronouns_without_s) {
-        console.log(`${k} ${i[0]}.`);
-    }
-    for (const k of pronouns_with_s) {
-        console.log(`${k} ${i[1]}.`);
-    }
+  for (const k of pronouns_without_s) {
+    console.log(`${k} ${i[0]}.`);
+  }
+  for (const k of pronouns_with_s) {
+    console.log(`${k} ${i[1]}.`);
+  }
 }
-//console.log(myFunction(2, 9));
